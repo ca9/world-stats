7fd2db86140828718ae6d9d9b5b3ea38f7deed5d
ca9 <Aditya11009@iiitd.ac.in>
2015-01-22 02:57:07 +0530

    Got basic repeat working.

--- static/js/main/choose.js
+++ static/js/main/choose.js
@@ -2,6 +2,17 @@
  * Created by aditya on 21/01/15.
  */
 
-angular.module('choose', []);
+var chooseApp = angular.module('chooseApp', []);
+
+chooseApp.controller('chooseController', function($scope){
+
+    $scope.range = function(min, max, step) {
+        step = step || 1;
+        var input = [];
+        for (var i = min; i <= max; i += step) input.push(i);
+        return input;
+    };
+
+});
 
 
