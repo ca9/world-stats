c4490317b6892848ee266ba0c1a62ff316ad101b
ca9 <Aditya11009@iiitd.ac.in>
2015-01-22 02:27:42 +0530

    Redid UI. Added angular. Made it work with Jinja (Flask-Triangle).

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
 from modules.home.main import home
+
+# Very important for Jinja to not clash with Angular JS
+from flask.ext.triangle import Triangle
+
 
 # Get app and Config
 app = Flask(__name__)
 app.config.from_object('config')
+
+#Lord save us from Jinja
+Triangle(app)
 
 # Blueprints
 app.register_blueprint(home)
---
+++ static/js/main/choose.js
@@ -0,0 +1,7 @@
+/**
+ * Created by aditya on 21/01/15.
+ */
+
+angular.module('choose', []);
+
+
