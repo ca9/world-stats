cfae34864efdf9fc6df89e9c8f757f345f96db17
aditya <Aditya11009@iiitd.ac.in>
2015-04-30 05:21:10 +0530

    Completes Map.

--- static/js/map/map.js
+++ static/js/map/map.js
@@ -7,8 +7,13 @@ worlDataApp.directive('worldMap', function() {
         template: "<div id='map-container'> " +
         "<fieldset style=\"text-align: center\">" +
          "<label for=\"map_year\"> Map Year: </label> &nbsp;&nbsp;" +
-         "<input ng-init=\"min_year\" ng-model=\"map_year\" id=\"year_choice\" type=\"range\" class=\"pure-u-2\" min=\"{{ min_year }}\" max=\"{{ max_year }}\"/>" +
-         "&nbsp; {{ map_year }}" +
+         "<input ng-init=\"min_year\" ng-model=\"map_year\" id=\"year_choice\" type=\"range\" class=\"pure-u-2\" min=\"{{ min_year }}\" max=\"{{ max_year }}\">" + "&nbsp; {{ map_year }}</input>"
+          + "<div class=\"pure-u-1-3\">" +
+            "<label for=\"indicator-map\"> &nbsp; Indicator </label>" +
+            "<select ng-model=\"map_var\">" +
+               "<option ng-repeat=\"item in mapvars\">{{ item }}</option>" +
+            "</select>" +
+         "</div>" +
         "</fieldset>" +
         "<div id='result-map' style='height: 400px' />" +
         "</div>",
@@ -27,10 +32,11 @@ worlDataApp.directive('worldMap', function() {
                     if (typeof(scope.mapDataInner) == typeof("abc"))
                         scope.mapDataInner = JSON.parse(scope.mapDataInner);
                     scope.map_var = Object.keys(scope.mapDataInner)[0];
+                    scope.mapvars = Object.keys(scope.mapDataInner);
                     scope.years = Object.keys(scope.mapDataInner[scope.map_var]);
                     scope.map_year = scope.years.sort()[0];
-                    scope.min_year = scope.years[0];
-                    scope.max_year = scope.years[scope.years.length - 1];
+                    scope.min_year = Number(scope.years[0]);
+                    scope.max_year = Number(scope.years[scope.years.length - 1]);
                     var myvalues = scope.mapDataInner[scope.map_var][scope.map_year];
                     $('#result-map').vectorMap({
                         //map: 'world-mill-en',
@@ -52,7 +58,7 @@ worlDataApp.directive('worldMap', function() {
             });
 
             scope.$watch(function(scope) {
-                    return scope.map_year;
+                    return scope.map_year + " - " + scope.map_var;
             },
             function(map_year, map_year_old) {
                 if (!jQuery.isEmptyObject(scope.mapDataInner)) {
